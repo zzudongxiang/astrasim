60291df66761aff3ffe28d0eff4f59d8057658b5
zzudongxiang <zzudongxiang@163.com>
2024-06-02 12:47:53 +0800

    删除子模块，添加对asplos2023的引用

---
+++ clone_astra_sim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Path
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+
+# 1. Clone ASTRA-sim
+(
+git clone --recursive https://github.com/astra-sim/astra-sim.git
+cd ${SCRIPT_DIR}/astra-sim/
+git checkout tags/tutorial-asplos2023
+git submodule update --init --recursive
+)
+
+# 2. Set analytical backend branch
+(
+cd ${SCRIPT_DIR}/astra-sim/extern/network_backend/analytical/
+git checkout tags/tutorial-asplos2023
+)
