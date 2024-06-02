24efd94b786dbd353c9ff667a20fe0bffd8edf5f
zzudongxiang <zzudongxiang@163.com>
2024-06-02 12:48:27 +0800

    添加ascend 910b仿真的参数

--- ascend_910b/run.sh
+++ ascend_910b/run.sh
@@ -2,27 +2,24 @@
 set -e
 
 # Path
-RUN_NAME="ASCEND_910B"
 SCRIPT_DIR="$(dirname "$(realpath $0)")"
-CONGESTION_AWARE_BIN="${SCRIPT_DIR}/../astra-sim/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Aware"
-CONGESTION_UNAWARE_BIN="${SCRIPT_DIR}/../astra-sim/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Unaware"
+BINARY="${SCRIPT_DIR}/../build/analytical/AnalyticalAstra/bin/AnalyticalAstra"
 RESULT_DIR="${SCRIPT_DIR}/log"
 
 # Inputs
-NETWORK="${SCRIPT_DIR}/inputs/network.yml"
-SYSTEM="${SCRIPT_DIR}/inputs/system.json"
-WORKLOAD="${SCRIPT_DIR}/inputs/two_comp_nodes_independent"
-MEMORY="${SCRIPT_DIR}/inputs/memory.json"
+NETWORK="${SCRIPT_DIR}/inputs/network/fullyconnected.json"
+SYSTEM="${SCRIPT_DIR}/inputs/system/fullyconnected.txt"
+WORKLOAD="${SCRIPT_DIR}/inputs/workload/all_reduce.txt"
+
 
 # 1. Setup
 rm -rf "${RESULT_DIR}"
 mkdir -p "${RESULT_DIR}"
 
 # 2. Run ASTRA-sim
-"${CONGESTION_UNAWARE_BIN}" \
-    --run-name="${RUN_NAME}" \
+"${BINARY}" \
+    --run-name="ascend" \
     --network-configuration="${NETWORK}" \
     --system-configuration="${SYSTEM}" \
     --workload-configuration="${WORKLOAD}" \
-    --remote-memory-configuration="${MEMORY}" \
     --path="${RESULT_DIR}/"
