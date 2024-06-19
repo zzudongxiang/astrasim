aa8d2b28da814002e0e2147ebb6eaa4a462adbe4
zzudongxiang <zzudongxiang@163.com>
2024-06-19 21:40:06 +0800

    添加ns3后端的编译脚本

--- script/build_ns3.sh
+++ script/build_ns3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$(realpath $0)")
-ROOT_DIR=${SCRIPT_DIR:?}/../astra-sim
+BUILD_DIR=${SCRIPT_DIR}/../build
+ROOT_DIR=${SCRIPT_DIR}/../astra-sim
 
 NS3_DIR=${ROOT_DIR}/extern/network_backend/ns-3
 CHARA_DIR=${ROOT_DIR}/extern/graph_frontend/chakra
@@ -13,3 +14,6 @@ protoc et_def.proto\
 cd ${NS3_DIR}
 ./ns3 configure --enable-mpi
 ./ns3 build AstraSimNetwork -j 12
+
+mkdir -p ${BUILD_DIR}
+mv ${NS3_DIR}/build/scratch/* ${BUILD_DIR}/
